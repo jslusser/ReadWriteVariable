833eb15223b03a071081c0ac214283587c752eb3
Jim Slusser <jslusser@kurkuma.co>
2023-01-15 20:25:17 -0800

    Update ReadWriteVariables.sol

--- ReadWriteVariables.sol
+++ ReadWriteVariables.sol
@@ -1,51 +1,49 @@
 // SPDX-License-Identifier: UNLICENSED
- pragma solidity ^0.8.4;
+pragma solidity ^0.8.4;
 
 contract ReadWriteVariables {
-    
-    int public Int;
-    uint256 public Uint;
-    string public String;
-    bool public Bool;
+    int256 Int;
+    uint256 Uint;
+    string String;
+    bool Bool;
 
-// Get/Set Int
+    // Get/Set Int
 
-    function getInt() public view returns(int) {
-    return Int;
-   }
+    function getInt() public view returns (int256) {
+        return Int;
+    }
 
-    function setInt(int _newInt) public {
+    function setInt(int256 _newInt) public {
         Int = _newInt;
     }
 
     // Get/Set Uint
 
-   function getUint() public view returns(uint256){
-    return Uint;
-   }
+    function getUint() public view returns (uint256) {
+        return Uint;
+    }
 
-    function setUint(uint256 _newUint)  public {
-       Uint= _newUint;
+    function setUint(uint256 _newUint) public {
+        Uint = _newUint;
     }
 
     // Get/Set String
 
-   function getString() public view returns(string memory){
-    return String;
-   }
+    function getString() public view returns (string memory) {
+        return String;
+    }
 
-   function setString(string memory _newString) public {
-    String = _newString;
-   }
+    function setString(string memory _newString) public {
+        String = _newString;
+    }
 
     // Get/Set Bool
 
-  function getBool() public view returns(bool){
-    return Bool;
-   }
+    function getBool() public view returns (bool) {
+        return Bool;
+    }
 
-   function setBool(bool _newBool) public {
-    Bool = _newBool;
-   }
-
+    function setBool(bool _newBool) public {
+        Bool = _newBool;
+    }
 }
